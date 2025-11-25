c7ae663b2a36ea6b0e7b3ee30d6855cb101ccaf8
alvarezke01 <alvarezke01@gmail.com>
2025-11-25 00:48:31 -0500

    feat(backend): add JWT auth endpoints to API

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("core.urls"))
 
+    # JWT auth endpoints
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path("", include("core.urls")),
 ]
